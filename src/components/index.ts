93756052af2123b42caf99fcd6355c40901b9664
shourovfoisal <shourovfoisal@gmail.com>
2023-09-22 22:40:11 +0600

    added chart boxes with recharts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from "./navbar/navbar.component";
 export { Menu } from "./menu/menu.component";
 export { Footer } from "./footer/footer.component";
 export { TopBox } from "./topBox/TopBox";
+export { ChartBox } from "./chartBox/ChartBox";
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,11 @@
-import { TopBox } from "@/components";
+import { ChartBox, TopBox } from "@/components";
 import "./home.scss";
+import {
+	chartBoxUser,
+	chartBoxProduct,
+	chartBoxRevenue,
+	chartBoxConversion,
+} from "@/data";
 
 export const Home = () => {
 	return (
@@ -7,11 +13,19 @@ export const Home = () => {
 			<div className="box box1">
 				<TopBox />
 			</div>
-			<div className="box box2">Box 2</div>
-			<div className="box box3">Box 3</div>
+			<div className="box box2">
+				<ChartBox {...chartBoxUser} />
+			</div>
+			<div className="box box3">
+				<ChartBox {...chartBoxProduct} />
+			</div>
 			<div className="box box4">Box 4</div>
-			<div className="box box5">Box 5</div>
-			<div className="box box6">Box 6</div>
+			<div className="box box5">
+				<ChartBox {...chartBoxConversion} />
+			</div>
+			<div className="box box6">
+				<ChartBox {...chartBoxRevenue} />
+			</div>
 			<div className="box box7">Box 7</div>
 			<div className="box box8">Box 8</div>
 			<div className="box box9">Box 9</div>
